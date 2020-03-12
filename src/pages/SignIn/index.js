0ffeede0c5408afe2d8f87fb18d05527527048b8
gabriel-fz <gabrielfzancanela@gmail.com>
2020-03-12 17:38:48 -0300

    tela de login e autenticaçao de adm completas

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/fastfeet-logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email(' ')
+    .required(' '),
+  password: Yup.string().required(' '),
+});
 
 export default function SignIn() {
-  return <h1>Olá, eu sou o Goku!</h1>;
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <strong>SEU E-MAIL</strong>
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
+        <strong>SUA SENHA</strong>
+        <Input name="password" type="password" placeholder="*************" />
+
+        <button type="submit">
+          {loading ? 'Carregando' : 'Entrar no sistema'}
+        </button>
+      </Form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,6 +1,56 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: #7d40e7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 360px;
+  max-height: 425px;
+  background: #fff;
+  padding: 60px 30px;
+  box-shadow: 0px 0px 10px #00000033;
+  border-radius: 4px;
+  opacity: 1;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+
+    input {
+      border: 1px solid #dddddd;
+      border-radius: 4px;
+      opacity: 1;
+      margin: 9px 0 15px;
+      height: 45px;
+
+      &::placeholder {
+        color: #999999;
+      }
+    }
+
+    button {
+      height: 45px;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      background: #7d40e7;
+      border-radius: 4px;
+      opacity: 1;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.05, '#7d40e7')};
+      }
+    }
+  }
 `;
