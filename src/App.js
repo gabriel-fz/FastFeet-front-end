b688762f2a259751a2e0c39d745c1fcb2b5dd246
gabriel-fz <gabrielfzancanela@gmail.com>
2020-03-12 10:42:20 -0300

    Arquivos e configuraçoes iniciais do ambiente

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+import history from './services/history';
+
+function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyle />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return <h1>Olá, eu sou o Goku!</h1>;
+}
